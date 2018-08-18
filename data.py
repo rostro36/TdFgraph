e945f25d853a4350f66ad048cb6f21b131d3d75b
rostro36 <rosenborg1996@hotmail.com>
2018-08-18 12:31:12 +0200

    Yapfed and nice

--- data.py
+++ data.py
@@ -1,18 +1,20 @@
-pedaleurs=[(None,None,None,None,None,None)]*220 #(NOM,PRENOM,JEUNE,NATION,EQUIPE,int(nombre))
-ecarts=[None]*23
-points=[None]*23
-grimpeurs=[None]*23
-jeunes=[None]*23
-eq=[None]*23
-ecarts[0]=[(0,0)]*220
-grimpeurs[0]=[(0,200)]*220
-points[0]=[(0,200)]*220
-jeunes[0]=[0]*220
-etape=0
-rankg=1
-rankj=1
-rankgr=1
-rankp=1
+pedaleurs = [(None, None, None, None, None, None)
+            ] * 220  #(NOM,PRENOM,JEUNE,NATION,EQUIPE,int(nombre))
+ecarts = [None] * 23
+points = [None] * 23
+grimpeurs = [None] * 23
+jeunes = [None] * 23
+eq = [None] * 23
+ecarts[0] = [(0, 0)] * 220
+grimpeurs[0] = [(0, 200)] * 220
+points[0] = [(0, 200)] * 220
+jeunes[0] = [0] * 220
+etape = 0
+rankg = 1
+rankj = 1
+rankgr = 1
+rankp = 1
+
 
 def newstage(etape1):
     global rankg
@@ -20,13 +22,13 @@ def newstage(etape1):
     global rankgr
     global rankp
     global etape
-    etape=etape1
-    rankg=1
-    rankj=1
-    rankgr=1
-    rankp=1
-    ecarts[etape]=[(None,None)]*220
-    grimpeurs[etape]=[(0,200)]*220
-    points[etape]=[(0,200)]*220
-    jeunes[etape]=[None]*220
-    eq[etape]=dict()
+    etape = etape1
+    rankg = 1
+    rankj = 1
+    rankgr = 1
+    rankp = 1
+    ecarts[etape] = [(None, None)] * 220
+    grimpeurs[etape] = [(0, 200)] * 220
+    points[etape] = [(0, 200)] * 220
+    jeunes[etape] = [None] * 220
+    eq[etape] = dict()
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 #libs used
 import PyPDF2 as pypdf
@@ -9,16 +8,16 @@ from graph import plot
 from data import newstage
 
 http = urllib3.PoolManager()
- #gather the data
-for etape in range(1,23): #change range back to 23?
-    etapestring=(str(hex(etape))[2:]).zfill(2)
-    print('working on stage:'+str(etape))
-    URL = 'http://azure.tissottiming.com/File/00031001070101'+etapestring+'FFFFFFFFFFFFFF00'
+#gather the data
+for etape in range(1, 23):
+    etapestring = (str(hex(etape))[2:]).zfill(2)
+    print('working on stage:' + str(etape))
+    URL = 'http://azure.tissottiming.com/File/00031001070101' + etapestring + 'FFFFFFFFFFFFFF00'
     #download the data
-    r = http.request('GET',URL)
-#   #check if press-release is ready/a PDF
-    if r.info()['Content-type']!='application/pdf':
-        print (str(etape)+' not ready')
+    r = http.request('GET', URL)
+    #check if press-release is ready/a PDF
+    if r.info()['Content-type'] != 'application/pdf':
+        print(str(etape) + ' not ready')
         break
     #open the file
     newstage(etape)
@@ -27,6 +26,6 @@ for etape in range(1,23): #change range back to 23?
     #process each page
     for pagenum in range(read_pdf.getNumPages()):
         process(read_pdf.getPage(pagenum).extractText())
-    print(str(etape)+' is processed')
+    print(str(etape) + ' is processed')
 print('processing done')
 plot(etape)
